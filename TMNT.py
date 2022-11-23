19cc005094b98da4f7dd702623e46a8dd434057b
Kalyn Williams <113442812+Kalyn543@users.noreply.github.com>
2022-11-23 10:13:36 -0500

    emoji

---
+++ TMNT.py
@@ -0,0 +1,52 @@
+from turtle import *
+
+def eye():
+    begin_fill()
+    circle(25)
+    end_fill()
+    color("black")
+    begin_fill()
+    circle(20)
+    end_fill()
+
+
+
+
+begin_fill()
+color("blue")
+
+circle(100)
+end_fill()
+color("white")
+left(90)
+penup()
+forward(100)
+left(90)
+forward(50)
+right(180)
+pendown()
+eye()
+color("white")
+penup()
+forward(100)
+pendown()
+eye()
+penup()
+begin_fill()
+color("black")
+left(180)
+forward(50)
+left(90)
+forward(75)
+left(90)
+pendown()
+begin_fill()
+circle(25)
+end_fill()
+color("red")
+begin_fill()
+circle(15)
+end_fill()
+
+
+input()
